33d41546956fa2134459c2686303942de21af4ce
Chidi <cokeke1@gmail.com>
2020-06-26 20:13:15 -0400

    update: css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Hello world</h1>
+    <h1>Image Classifier</h1>
+    <h3>Upload an image and see what it is classified as. This demo uses Tensorflow.js and neural networks to predict and classify your image.</h3>
+    <button>Upload</button>
     </div>
   );
 }
